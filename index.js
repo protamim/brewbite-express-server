a3e4ed806dca51661dbe0d04fb2304cb63871626
Tamim <s163.tamim@gmail.com>
2023-10-21 00:35:31 +0600

    created number of API

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 // dotenv configuration
@@ -33,6 +33,7 @@ async function run() {
 
     const database = client.db('brewBite');
     const prodCollection = database.collection('products');
+    const userCollection = database.collection('users');
 
     // find multiple document by brand name
     app.get('/products/:brand', async(req, res)=> {
@@ -61,12 +62,43 @@ async function run() {
       const product = await prodCollection.findOne(query);
       res.send(product);
     })
+
+    // Find user selected data
+    app.get('/user/cart', async(req, res)=> {
+      const selectedProducts = userCollection.find();
+      const result = await selectedProducts.toArray();
+      res.send(result);
+    })
+
+    // Find data with a specific id on the cart API
+    app.get('/user/cart/:id', async(req, res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.findOne(query);
+      res.send(result)
+    })
     
     // Insert document/data to database from client side 
     app.post('/products', async(req, res)=> {
         const product = req.body;
         const result = await prodCollection.insertOne(product)
         res.send(result);
+    })
+
+    // Insert user data when user click to Add to cart
+    app.post('/user/cart', async(req, res)=> {
+      const userInfo = req.body;
+      const arr = [userInfo]
+      const result = await userCollection.insertMany(arr);
+      res.send(result);
+    })
+
+    // Delete operation in the my cart
+    app.delete('/user/cart/:id', async(req, res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
     })
 
 
